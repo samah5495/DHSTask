c61bd4e2b2de30992ac0d989d9f0ada75fdf41b6
samahhamdy1895@yahoo.com <samahhamdy1895@yahoo.com>
2020-04-08 23:39:11 +0200

    DHS

---
+++ API/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace API.Models
+{
+    public class Product
+    {
+         [Key]
+         public int ProductId { get; set; }
+
+        [Required]
+         public string ProductName { get; set; } 
+         public float ProductPrice { get; set; }
+         public string ProductCode { get; set; }
+    }
+}
