940828f8088a82956ea18035797bdcbabc556f3c
memokhial@gmail.com <memokhial@gmail.com>
2020-04-08 23:29:19 +0200

    DHS  Task Commit

---
+++ API/Migrations/202004051831554_first.cs
@@ -0,0 +1,28 @@
+﻿namespace API.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class first : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ProductId = c.Int(nullable: false, identity: true),
+                        ProductName = c.String(nullable: false),
+                        ProductPrice = c.Single(nullable: false),
+                        ProductCode = c.String(),
+                    })
+                .PrimaryKey(t => t.ProductId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+        }
+    }
+}
---
+++ MVCHost/Models/ProductViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCHost.Models
+{
+    public class ProductViewModel
+    {
+        
+        public int ProductId { get; set; }
+
+       
+        public string ProductName { get; set; }
+        public float ProductPrice { get; set; }
+        public string ProductCode { get; set; }
+    }
+}
